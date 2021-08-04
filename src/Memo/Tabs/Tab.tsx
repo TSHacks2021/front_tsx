8d37d79f97aecf264265c8bd396e68c9412ac8ac
t-fuji109 <fujita-tomoko-sz@ynu.jp>
2021-08-04 12:53:47 +0900

    コンポーネントの横配置

--- src/Memo/Tabs/Tab.tsx
+++ src/Memo/Tabs/Tab.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import "./Tab.css"
 
 type TabProps = {
     title: string
 }
 
 const Tab: React.FC<TabProps> = ({children}) => {
-    return <div>{children}</div>
+    return (
+        <div className="box">
+            <div className="Memo">{children}</div>
+            <div className="Memo">{children}</div>
+        </div>
+
+    );
 }
 
 export default Tab
