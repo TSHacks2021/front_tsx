5263b2d3baece4640a7070beba7110b7c0dd5449
Koki Ota <ota-koki-gy@ynu.jp>
2021-08-04 11:38:22 +0900

    Modal追加，設定ボタンでモーダルが開く

--- src/Time/SettingTime.tsx
+++ src/Time/SettingTime.tsx
@@ -1,11 +1,30 @@
 import React, { useEffect } from "react";
+import Modal from "react-modal"; //npm install --save react-modal @types/react-modal でインストール
+
+Modal.setAppElement("#root");
 
 function SettingTime(){
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+
   return (
     <div>
-      <button>
+      <button
+        onClick={()=>{
+          setIsOpen(true)
+        }}  
+      >
         設定
       </button>
+      <Modal isOpen={modalIsOpen}>
+        <button
+          onClick={()=>{
+            setIsOpen(false)
+          }}  
+        >
+          閉じる
+        </button>
+      </Modal>
     </div>
   );
 }
