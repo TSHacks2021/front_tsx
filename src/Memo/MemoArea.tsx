3da95b2af845d9f52d202351a9d95f687cdf6cd5
t-fuji109 <fujita-tomoko-sz@ynu.jp>
2021-08-04 14:22:40 +0900

    ダミーデータの作成とメモエリアの基本形

---
+++ src/Memo/MemoArea.tsx
@@ -0,0 +1,64 @@
+import React, {useState} from "react";
+import Tabs from "./Tabs/Tabs"
+import Tab from "./Tabs/Tab"
+import Memo from "./Memo";
+import { TodayPresenter } from "./TodayPresenter";
+import PresenterTab from "./PresenterTab"
+//function SampleMemo() {
+
+const dummyPresenters: TodayPresenter[] = [
+    {
+        id: 1,
+        name: "A",
+        memo: "a"
+    },
+    {
+        id: 2,
+        name: "B",
+        memo: "b"
+    },
+    {
+        id: 3,
+        name: "C",
+        memo: "c"
+    },
+    {
+        id: 4,
+        name: "D",
+        memo: "d"
+    },
+];
+
+const MemoArea: React.FC = () => {
+    const[presenters, setPresenters] = useState(dummyPresenters);
+
+    const handleMemoChange = (id: number, memo: string) => {
+        const newPresenters = presenters.map((p) => {
+            return p.id === id
+                ? {...p, memo:memo}
+                :p;
+        });
+        setPresenters(newPresenters);
+    };
+
+    const presenterTabs = presenters.map((p) => {
+        return(
+            <PresenterTab
+                presenter={p}
+                key={p.id}
+                onMemoChange={(id, memo) => handleMemoChange(id, memo)}
+                //onMemoChange={(id, memo) => {}}
+            />
+        );
+    });
+
+    return(
+        <div>
+            <Tabs>
+                {presenterTabs}
+            </Tabs>
+        </div>
+    );
+}
+
+export default MemoArea;
