fe6c85f2bff4117313bb755dd4eb0c82db50131e
t-fuji109 <fujita-tomoko-sz@ynu.jp>
2021-08-05 14:45:00 +0900

    分からない，強制的にレンダリング

--- src/Memo/Tabs/Chat/ChatArea.tsx
+++ src/Memo/Tabs/Chat/ChatArea.tsx
@@ -1,4 +1,4 @@
-import React, {useState, Component} from "react";
+import React, {useState, Component, useEffect} from "react";
 import { isPropertySignature } from "typescript";
 
 //import {connect} from 'react-redux'
@@ -25,6 +25,8 @@ type ChatAreaProps = {
 const ChatArea = (props:ChatAreaProps) => {
     //const chats = props.chats
     const[chats, setChats] = useState(props.chats);
+    //boolean型のstateを作成
+    const [update,setUpdata]=useState<boolean>(false)
     let newchat: string;
     const handleChatChange = (chat: string) => {
         newchat = chat
@@ -35,6 +37,9 @@ const ChatArea = (props:ChatAreaProps) => {
         const newChats = chats
         newChats.push(newchat)
         setChats(newChats)
+
+        //レンダリングしたい場所でこれを差し込むだけ
+        setUpdata(update?false:true)
         console.log(props.chats)
     }
 
