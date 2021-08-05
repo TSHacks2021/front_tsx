10ba9c842f68c633ca838c597513462a1f56e5f3
t-fuji109 <fujita-tomoko-sz@ynu.jp>
2021-08-05 11:55:04 +0900

    chatの追加

--- src/Memo/PresenterTab.tsx
+++ src/Memo/PresenterTab.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { TodayPresenter } from "./TodayPresenter";
-import Tab from "./Tabs/Tab"
-import "./PresenterTab.css"
+import Tab from "./Tabs/Tab";
+import "./PresenterTab.css";
+import ChatArea from "./Tabs/Chat/ChatArea";
 
 type PresenterTabProps = {
     presenter: TodayPresenter;
@@ -9,7 +10,7 @@ type PresenterTabProps = {
 };
 
 const PresenterTab = (props:PresenterTabProps) => {
-    const {name, memo} = props.presenter;
+    const {name, memo, chats} = props.presenter;
 
     const handleMemoChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         props.onMemoChange(props.presenter.id, e.target.value);
@@ -28,7 +29,11 @@ const PresenterTab = (props:PresenterTabProps) => {
                     />
                 </div>
                 <div className="field">
-                    <input></input>
+                    {/*<textarea
+                        value={chats}/>*/}
+                    <ChatArea
+                        chats={chats}/*value={chats}*/
+                    />
                 </div>
                 </div>
             </Tab>
