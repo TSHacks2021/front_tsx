7e141bed5a18489dd9cd36310285f375aa98a16d
t-fuji109 <fujita-tomoko-sz@ynu.jp>
2021-08-05 17:57:16 +0900

    色変更

--- src/Memo/Tabs/TabTitle.tsx
+++ src/Memo/Tabs/TabTitle.tsx
@@ -5,16 +5,27 @@ type TabTitleProps = {
     title: string
     index: number
     setSelectedTab: (index: number) => void
+    isSelected: boolean
 }
 
-const TabTitle: React.FC<TabTitleProps> = ({title, setSelectedTab, index}) => {
+const TabTitle: React.FC<TabTitleProps> = ({title, setSelectedTab, index, isSelected}) => {
     const onClick = useCallback(() => {
-        setSelectedTab(index)
+        setSelectedTab(index)    
     }, [setSelectedTab, index])
+    //const isSelected = props.index
     
     return(
         <li>
-            <button onClick={onClick}>{title}</button>
+            {
+                (() => {
+                    if(isSelected) {
+                        return <button className="selected" onClick={onClick}>{title}</button>
+                    } else {
+                        return <button className="notSelected" onClick={onClick}>{title}</button>
+                    }
+                })()
+            }
+            {/*<button onClick={onClick}>{title}</button>*/}
         </li>
     )
 }
