f59fbdb1ceb5e38a7c2595e7fbdc18ea2ff346d1
t-fuji109 <fujita-tomoko-sz@ynu.jp>
2021-08-06 16:01:17 +0900

    いろいろ修正

--- src/Memo/MemoArea.tsx
+++ src/Memo/MemoArea.tsx
@@ -42,17 +42,33 @@ type MemoAreaProps = {
     presenters: string[];
     presenterNum: number;
     socket: Socket;
+    timeInfo: TimeInfo;
+}
+var dummypresenters: TodayPresenter[]
+var savedummypresenters: TodayPresenter[] = new Array(0)
+var checksetMessage: any = null
+
+const sendMessage = (props: MemoAreaProps, presentername: string, sendmessage: string) => {
+    var message = {"messagetype":"memo", "presentername": presentername, "message": sendmessage};
+    //var message = {"messagetype":"memo", "message": sendmessage};
+    //var message = {"messagetype":"message"};
+    var mes_json = JSON.stringify(message);
+    console.log(mes_json);
+    props.socket.emit(mes_json);
 }
 
 const MemoArea = (props: MemoAreaProps) => {
     //const[presenters, setPresenters] = useState(dummyPresenters);
-    var dummypresenters: TodayPresenter[] = new Array(props.presenterNum)
-
-    for(var i = 0; i < props.presenterNum;i++) {
-        dummypresenters[i] = {id:i,name:props.presenters[i],memo:"",chats:[]}
+    dummypresenters = savedummypresenters.slice()
+    if (savedummypresenters.length == 0) {
+        for(var i = 0; i < props.presenterNum;i++) {
+            dummypresenters[i] = {id:i,name:props.presenters[i],memo:"",chats:[]}
+        }
     }
+
     const[presenters, setPresenters] = useState(dummypresenters)
-    props.socket.on("message", receiveMessage);
+    const [newMessage, setNewMessage] = useState(props.timeInfo.getChatMessage())
+    /*props.socket.on("message", receiveMessage);
 
     function receiveMessage(e:any){
         let message = JSON.parse(e.data);
@@ -66,7 +82,30 @@ const MemoArea = (props: MemoAreaProps) => {
             }
         });
         setPresenters(newPresenters);
-    }
+    }*/
+    if (checksetMessage) clearInterval(checksetMessage);
+    checksetMessage = setInterval(function(){setNewMessage(props.timeInfo.getChatMessage())}, 100);
+
+    React.useEffect(() => {
+        const newPresenters = presenters.map((p) => {
+            /*
+            if (p.id == number(newMessage["to"])) {
+                p.chats.push(newMessage["sender"]+": "+newMessage["message"])
+                return{...p, chats:p.chats}
+            } else {
+                return p;
+            }*/
+            
+            if (p.id == 0) {
+                p.chats.push(newMessage["message"])
+                return{...p, chats:p.chats}
+            } else {
+                return p;
+            }
+        });
+        setPresenters(newPresenters);
+        savedummypresenters = newPresenters.slice()
+    },[newMessage])
 
     const handleMemoChange = (id: number, memo: string) => {
         const newPresenters = presenters.map((p) => {
@@ -75,22 +114,14 @@ const MemoArea = (props: MemoAreaProps) => {
                 :p;
         });
         setPresenters(newPresenters);
+        savedummypresenters = newPresenters.slice()
     };
 
     const handleSendButtonClick = (presentername: string, sendmessage: string) => {
         //送信の処理ができればここでする？
-        sendMessage(presentername, sendmessage)        
+        sendMessage(props, presentername, sendmessage)        
     };
     
-    const sendMessage = (presentername: string, sendmessage: string) => {
-        var message = {"messagetype":"memo", "presentername": presentername, "message": sendmessage};
-        //var message = {"messagetype":"memo", "message": sendmessage};
-        //var message = {"messagetype":"message"};
-        var mes_json = JSON.stringify(message);
-        console.log(mes_json);
-        props.socket.emit(mes_json);
-    }
-
     const presenterTabs = presenters.map((p) => {
         return(
             <PresenterTab
