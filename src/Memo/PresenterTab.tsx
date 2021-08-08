a9d0ffc3c3501e6c745f916d15474c4461e32357
t-fuji109 <fujita-tomoko-sz@ynu.jp>
2021-08-08 18:45:12 +0900

    不要箇所の削除，コメントの追加

--- src/Memo/PresenterTab.tsx
+++ src/Memo/PresenterTab.tsx
@@ -7,21 +7,23 @@ import ChatArea from "./Tabs/Chat/ChatArea";
 type PresenterTabProps = {
     presenter: TodayPresenter;
     onPrivateMemoChange: (id: number, memo: string) => void;
-
     onSendButtonClick: (presenterName:string, message: string) => void;
 };
 
 const PresenterTab = (props:PresenterTabProps) => {
     const {name, memo, chats} = props.presenter;
 
+    //PrivateMemoが変更されたら
     const handlePrivateMemoChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         props.onPrivateMemoChange(props.presenter.id, e.target.value);
     };
 
+    //PublicMemoの送信ボタンが押されたら
     const handleSendButtonClick = (message: string) => {
         props.onSendButtonClick(props.presenter.name, message);
     };
 
+    //Tab.tsx, ChatArea.tsxへ
     return(
         <div className="presenter-tab">
             <Tab title="a">
