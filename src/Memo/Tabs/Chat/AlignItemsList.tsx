2ae87998a1c512d4fb3a1d2977147ecc40c037ba
t-fuji109 <fujita-tomoko-sz@ynu.jp>
2021-08-08 18:49:43 +0900

    不要箇所の削除

--- src/Memo/Tabs/Chat/AlignItemsList.tsx
+++ src/Memo/Tabs/Chat/AlignItemsList.tsx
@@ -1,12 +1,8 @@
-  
-
 import React, {useState} from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
 import ListItemText from '@material-ui/core/ListItemText';
-
 
 import "./AlignItemsList.css"
 
