5c49169019e26d5e66fb06693e48328c2a0d7f2d
t-fuji109 <fujita-tomoko-sz@ynu.jp>
2021-08-26 18:08:22 +0900

    コメントの追加

--- src/ConferenceTab.tsx
+++ src/ConferenceTab.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-//import { TodayPresenter } from "./TodayPresenter";
 import ConTab from "./ConTab"
-//import "./PresenterTab.css"
-//import ChatArea from "./Tabs/Chat/ChatArea";
 import Time from './Time/Time';
 import Memo from './Memo/Memo';
 import { TimeInfo } from './TimeInfo';
@@ -14,28 +11,20 @@ type ConferenceTabProps = {
 };
 
 const ConferenceTab = (props:ConferenceTabProps) => {
-    //const {name, privateMemo, chats} = props.presenter;
-
-    //PrivateMemoが変更されたら
-    /*
-    const handlePrivateMemoChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        props.onPrivateMemoChange(props.presenter.id, e.target.value);
-    };*/
-
-    //PublicMemoの送信ボタンが押されたら
-    /*
-    const handleSendButtonClick = (name: string, message: string) => {
-        props.onSendButtonClick(props.presenter.id, name, message);
-    };*/
-
-    //Tab.tsx, ChatArea.tsxへ
+    // デザインなど，全く拘っていないので調整する際には，似た名前のTabとかTabsとかのcssを参考にすること
+    // タブの内容をここに持っておく
+    /**
+     * 試しに動かしてみる際には，<Time>を一度消す
+     * タブのボタンを触れるようにするためには，timebarの位置を調整し直す必要がある
+     * このままだと，触れない
+     */
     return(
         <div className="presenter-tab">
             <ConTab title="a">
             <Time timeInfo={props.timeInfo}/>
 
             <Memo timeInfo={props.timeInfo}
-            socket={props.socket}/>
+                    socket={props.socket}/>
 
             </ConTab>
         </div>
